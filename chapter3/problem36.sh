00b250102c8c3726b676cf117fccb5d196ca0297
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-05-19 06:47:24 +0900

    chapter3 problem36

--- chapter3/problem36.sh
+++ chapter3/problem36.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cat ../shellgei160/qdata/36/message.txt | sed 's/)(/)\n(/g'| grep -P '^(\(\g<1>\)|[^()]+)$' | tr -d '()' | paste - -sd ''
+
+cat ../shellgei160/qdata/36/message.txt | sed -E 's/\)\(/)\n(/g' | awk '{print gsub("\\(", ""), gsub("\\)", ""), $0}' | awk '$1==$2{printf $3}' | xargs
