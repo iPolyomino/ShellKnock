e5eb16b549bfc197fe7784a44f067ab0c3db0390
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-05-29 13:06:42 +0900

    add: problem37 another solution

--- chapter3/problem37.sh
+++ chapter3/problem37.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cat ../shellgei160/qdata/37/diarydiary.txt | tr -d '\n' | grep -oE '(.+)\1'
+
+cat ../shellgei160/qdata/37/diarydiary.txt | sed -rz 's/(.+)(\n*)\1/@\1\1@/g' | grep -o '@[^@]*@' | tr -d @
