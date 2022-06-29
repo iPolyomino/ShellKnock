d9f2b920b7cf2606e2a79d9dc2760141fe73e8c1
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-06-29 20:25:06 +0900

    chapter3 problem43 another solution

--- chapter3/problem43.sh
+++ chapter3/problem43.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 cat ../shellgei160/qdata/43/bunken.txt | awk 'NR%3==1{k=$1};{print k,$0}' | sort -s -k1,1 | sed 's/....//'
+
+cat ../shellgei160/qdata/43/bunken.txt | tr '\n' '@' | sed 's/@@/@\n/g' | sort | tr '@' '\n'
+
+cat ../shellgei160/qdata/43/bunken.txt | sed 's/\[/\x0&/g' | sort -z | tr -d '\0'
