373bfb73ea3e89f1f43462e0deed5d3a76ca3632
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-07-01 19:21:20 +0900

    chapter3 problem44 another solution

--- chapter3/problem44.sh
+++ chapter3/problem44.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 cat ../shellgei160/qdata/44/diary.txt | sed -Ez 's/シ.?ェ.?ル.?ス.?ク.?リ.?プ.?ト/%&%/g' | sed '/%/s/$/ @/;s/%//g'
+
+
+awk '{a[NR]=$0;b[NR]=gensub(/シェルスクリプト/,"","g",$0)}END{for(i=1;i<=NR;i++){print a[i],b[i-1]a[i]b[i+1]}}' ../shellgei160/qdata/44/diary.txt | awk '{print $1,/シェルスクリプト/?"@":""}'
