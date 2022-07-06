d795879d2347a26faf4fb9305c4c7ab469a5c3a4
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-07-06 12:42:14 +0900

    chapter4 practice 4.1.a 3

--- chapter4/practice/4.1.a.sh
+++ chapter4/practice/4.1.a.sh
@@ -3,3 +3,5 @@
 cat ../../shellgei160/qdata/practice/table.txt | awk '{print $0,$2+$3}'
 
 cat ../../shellgei160/qdata/practice/table.txt | awk '{print;a+=$2;b+=$3}END{print "計",a,b}'
+
+cat ../../shellgei160/qdata/practice/table.txt | awk '{print;x[$1]+=$2;y[$1]+=$3}END{for(k in x){print k"計",x[k],y[k]}}'
