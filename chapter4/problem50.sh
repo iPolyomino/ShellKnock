19322b46f219986f9b266dc1d6346cc5c3d0e365
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-07-18 13:17:19 +0900

    chapter4 problem50

---
+++ chapter4/problem50.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+awk '{a[$3$4]+=$5}END{for(k in a)print k,a[k]}' ../shellgei160/qdata/50/sales | sort | join <(sed 's/ //' ../shellgei160/qdata/50/stones_master) - | awk '{print $2,$3*$4}' | column -t
