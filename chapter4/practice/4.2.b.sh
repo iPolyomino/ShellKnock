196156103a4da07a489e8c496dd5384176e57768
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-07-27 22:34:30 +0900

    chapter4 practice 4.2.b another solution

--- chapter4/practice/4.2.b.sh
+++ chapter4/practice/4.2.b.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 nkf -wLux ../../shellgei160/qdata/practice/data.csv | ruby -r csv -ne 'CSV($_).each{|row| puts row[1]}'
+
+nkf -wLux ../../shellgei160/qdata/practice/data.csv | python3 -c 'import csv,sys;[print(x[1]) for x in csv.reader(sys. stdin)]'
