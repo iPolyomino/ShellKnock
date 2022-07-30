53d410204b8cfeaab827cfa4ac8743e9b4b08bf2
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-07-31 06:00:29 +0900

    chapter4 problem56

---
+++ chapter4/problem56.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -E 's;^(.*):(.*);s/\1/\2/g;g' ../shellgei160/qdata/56/service_stop_weekday.txt | sed -f - ../shellgei160/qdata/56/service_depend_list.txt | tr -d :, | awk '{for(i=2;i<=NF;i++)print $i,$1}' | sort -u | awk '{a[$1]=a[$1]" "$2}END{for(k in a)print k":"a[k]}' | sed 'y/日月火水木金土/1234567/' | sort | sed 'y/1234567/日月火水木金土/' | sed 's/ /, /g;s/,//'
