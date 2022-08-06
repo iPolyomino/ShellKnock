b8a75f0b9fc43ff9376f8c78aa1cfa22edf880f4
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-08-06 21:53:00 +0900

    chapter4 problem56 another solution

--- chapter4/problem56.sh
+++ chapter4/problem56.sh
@@ -4,3 +4,5 @@ sed -E 's;^(.*):(.*);s/\1/\2/g;g' ../shellgei160/qdata/56/service_stop_weekday.t
 
 
 grep -o . <<<日月火水木金土 | while read s;do grep $s ../shellgei160/qdata/56/service_stop_weekday.txt | cut -d : -f1 | grep -f- ../shellgei160/qdata/56/service_depend_list.txt | cut -d: -f1 | xargs | sed 's/ /, /g' | sed "s/^/$s: /" ;done
+
+cat ../shellgei160/qdata/56/service_depend_list.txt | tr -d ':,' | awk '{for(i=2;i<=NF;i++)print $1,$i}' | sort -k2,2 | join -1 2 -2 1 - <(cat ../shellgei160/qdata/56/service_stop_weekday.txt | tr -d ':,' | sort -s -k1,1) | awk '{for(i= 3;i<=NF;i++)print $i,$2}' | sort | uniq | join -a 1 - <(echo 日月火水木金土 | grep -o . | awk '{print $0,NR}' | sort) | sort -s -k3,3 | awk 'pre!=$1{printf "\n"$1": "}{printf $2", ";pre=$1}' | awk NF | sed 's/, *$//'
