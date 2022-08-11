dc765f173f9c98892232930f98330943122d0809
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-08-11 21:09:20 +0900

    chapter4 problem59 another solution

--- chapter4/problem59.sh
+++ chapter4/problem59.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 cat ../shellgei160/qdata/59/data.csv | grep -Eno '[^,"]*|"([^"]*("")*)*"' | sed 's/:.*//' | uniq -c | awk '$1==2{print $2}'
+
+python3 -c 'import csv;f=open("../shellgei160/qdata/59/data.csv","r");[print("|".join(x)) for x in csv.reader(f) ]' | awk -F'|' 'NF==2{print NR}'
