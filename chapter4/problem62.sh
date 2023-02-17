217ee080a9f6ab569dcfe7b44b2281dc4d25f4a7
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2023-02-18 06:18:37 +0900

    chapter4 problem62

---
+++ chapter4/problem62.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nkf -wLux ../shellgei160/qdata/62/syukujitsu.csv | tail -n +2 | teip -d, -f 1 -- date -f- '+%Y-%m-%d' | awk -F- '$1>=2019&&$1<2022' | cat - <(dateutils.dseq 2019-01-01 2021-12-31 | sed 's/$/,@/') | sort -r | uniq -w10 | tac
